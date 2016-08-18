86036d22c1e31cd0797ce254b23582adaba2b4f3
jakobotten <jakob.otten@my.jcu.edu.au>
2016-08-18 11:15:45 +1000

    Add functions to test_written_code for getting input from the user and printing the output

--- Prac03/test_written_code.py
+++ Prac03/test_written_code.py
@@ -1,7 +1,22 @@
-user_name = input("What is your name: ")
-user_name.replace(" ","")
-while user_name == "":
-    print("Name can't be blank")
-    user_name = input("What is your name:")
-for i in range(0,len(user_name),2):
-    print(user_name[i],end="")
+
+
+def main():
+    user_name = get_name()
+    letter_frequency = int(input("Enter letter frequency: "))
+    print_name(user_name, letter_frequency)
+
+
+def print_name(user_name, frequency_of_letters):
+    for i in range(0, len(user_name), frequency_of_letters):
+        print(user_name[i], end="")
+
+
+def get_name():
+    user_name = input("Enter name: ")
+    user_name.replace(" ", "")
+    while user_name == "":
+        print("Name can't be blank")
+        user_name = input("Enter name:")
+    return user_name
+
+main()
