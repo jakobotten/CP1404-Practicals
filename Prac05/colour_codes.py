a0a605523d5b5dc34f76704cbb2a0b0d3c9e4549
jakobotten <jakob.otten@my.jcu.edu.au>
2016-09-01 11:00:06 +1000

    Divide into functions

--- Prac05/colour_codes.py
+++ Prac05/colour_codes.py
@@ -1,11 +1,14 @@
-COLOUR_HEX_VALUE = {"BROWN": "#a52a2a", "CHOCOLATE": "#d2691e", "BURLYWOOD": "#deb887", "DARKORANGE": "#ff8c00",
-                    "TAN": "#d2b48c", "SADDLEBROWN": "#8b4513", "OLIVEDRAB": "#6b8e23", "INDIANRED": "#cd5c5c",
-                    "HOTPINK": "#ff69b4", "GRAY": "#bebebe"}
+def main():
+    COLOUR_HEX_VALUE = {"BROWN": "#a52a2a", "CHOCOLATE": "#d2691e", "BURLYWOOD": "#deb887", "DARKORANGE": "#ff8c00",
+                        "TAN": "#d2b48c", "SADDLEBROWN": "#8b4513", "OLIVEDRAB": "#6b8e23", "INDIANRED": "#cd5c5c",
+                        "HOTPINK": "#ff69b4", "GRAY": "#bebebe"}
+    colour = input("Enter colour name: ").upper()
+    while colour != "":
+        if colour in COLOUR_HEX_VALUE:
+            print("Hex value for the colour", colour.capitalize(), "is", COLOUR_HEX_VALUE[colour])
+        else:
+            print("Invalid colour name")
+        state = input("Enter colour name: ").upper()
 
-colour = input("Enter colour name: ").upper()
-while colour != "":
-    if colour in COLOUR_HEX_VALUE:
-        print("Hex value for the colour", colour.capitalize(), "is", COLOUR_HEX_VALUE[colour])
-    else:
-        print("Invalid colour name")
-    state = input("Enter colour name: ").upper()
+
+main()
--- Prac05/occurrences_of_words.py
+++ Prac05/occurrences_of_words.py
@@ -1,21 +1,27 @@
-user_string = input("Enter a sentence: ")
-word_tally = {}
+def main():
+    user_string = input("Enter a sentence: ")
+    word_tally = {}
+    words = user_string.split()
 
-words = user_string.split()
+    for word in words:
+        if word in word_tally:
+            word_tally[word] += 1
+        else:
+            word_tally[word] = 1
 
-for word in words:
-    if word in word_tally:
-        word_tally[word] += 1
-    else:
-        word_tally[word] = 1
+    display_word_tally(word_tally)
 
-list_of_words = []
-longest_word = 0
-for key in word_tally:
-    list_of_words += [key]
-    if len(key) >= longest_word:
-        longest_word = len(key)
 
-list_of_words.sort()
-for word in list_of_words:
-    print("{0:{1}} : {2}".format(word,longest_word,word_tally[word]))
+def display_word_tally(word_tally):
+    list_of_words = []
+    longest_word = 0
+    for key in word_tally:
+        list_of_words += [key]
+        if len(key) >= longest_word:
+            longest_word = len(key)
+    list_of_words.sort()
+    for word in list_of_words:
+        print("{0:{1}} : {2}".format(word, longest_word, word_tally[word]))
+
+
+main()
